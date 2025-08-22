b554695db69ce460a11208606bcc59aa0e55aca8
Anas Asif <muhammadanasasif85@gmail.com>
2025-08-23 02:22:29 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import WordScramble from './components/WordScramble';
+
+function App() {
+  return (
+    <div>
+      <WordScramble />
+    </div>
+  );
+}
+
+export default App;
